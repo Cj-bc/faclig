7e412d9222b3161eb1bb05b29cf7d0f364e50c04
Cj-bc <cj-bc@y-modify.org>
2018-10-09 18:42:11 +0900

    Add: asset/parts dictionaries, support multiple cascades [WIP]

--- eye_tracking.py
+++ eye_tracking.py
@@ -5,6 +5,25 @@
 import cv2
 import time
 import shutil
+asset = {
+        "asset_root": "src",
+        "eye": "eye.txt",
+        "mouth": "mouth.txt"
+        }
+
+cascade_file_root = "haarcascades"
+parts = [{
+            "part": "eye",
+            "cascade_file": "haarcascade_eye.xml"},
+         {
+             "part": "eye",
+             "cascade_file": "haarcascade_lefteye_2splits.xml"},
+         {
+             "part": "mouth",
+             "cascade_file": ""}
+         ]
+
+
 
 if __name__ == '__main__':
     ESC_KEY = 27
@@ -16,8 +35,10 @@ if __name__ == '__main__':
 
     DEVICE_ID = 0
 
-    cascade_file = "haarcascades/haarcascade_eye.xml"
-    cascade = cv2.CascadeClassifier(cascade_file)
+    # add cascade object to each dict in parts
+    parts = [part.update({"cascade":
+                         cv2.CascadeClassifier(f'{cascade_file_root}/{part.cascade_file}')})
+             for part in parts]
 
     cap = cv2.VideoCapture(DEVICE_ID)
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -32,18 +53,18 @@ if __name__ == '__main__':
 
         img = c_frame
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face_list = cascade.detectMultiScale(img_gray, minSize=(100, 100))
-
-        # x: left > right
-        # y: bottom > top
-        for (x, y, w, h) in face_list:
-            x_percent = round(x / width * 100)
-            y_percent = round(y / height * 100)
-            print(f'x:{x_percent}, y:{y_percent}')
-            time.sleep(0.5)
 
         cv2.imshow(ORG_WINDOW_NAME, c_frame)
+        for  part in parts:
+            face_list = part.cascade.detectMultiScale(img_gray, minSize=(100, 100))
 
+            # x: left > right
+            # y: bottom > top
+            for (x, y, w, h) in face_list:
+                x_percent = round(x / width * 100)
+                y_percent = round(y / height * 100)
+                os.system(f'bash ./draw.sh {x_percent} {y_percent} {asset["asset_root"]}/{asset[part.part]}')
+                time.sleep(0.5)
         key = cv2.waitKey(INTERVAL)
         if key == ESC_KEY:
             break
