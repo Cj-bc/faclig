bb24a0c73573f12a93e5c08c6640c11426a88ac4
Cj-bc <cj.bc-sd@outlook.jp>
2020-03-12 18:34:36 +0900

    Change: add helpful annotations for FilePath's FromJSON instance

--- src/Graphics/Asciiart/Faclig/Types/Internal.hs
+++ src/Graphics/Asciiart/Faclig/Types/Internal.hs
@@ -4,9 +4,13 @@ module Graphics.Asciiart.Faclig.Types.Internal where
 
 
 import Control.Lens (makeLenses, (^.))
+import Control.Monad (when)
 import Shgif.Type (Shgif)
 import Data.Yaml
+import Data.Aeson ((<?>))
+import Data.Aeson.Internal (JSONPathElement(Key))
 import Data.HashMap.Lazy ((!))
+import qualified Data.Vector as V
 
 type Offset = (Int, Int)
 type Part = (Shgif, Offset)
@@ -31,20 +35,21 @@ makeLenses ''FaceFile
 
 instance FromJSON FaceFile where
     parseJSON = withObject "Face" $ \v -> do
-                    parts <- v .: "parts" :: Parser Object
-                    FaceFile <$> parsePart (parts ! "contour")
-                             <*> parsePart (parts ! "leftEye")
-                             <*> parsePart (parts ! "rightEye")
-                             <*> parsePart (parts ! "nose")
-                             <*> parsePart (parts ! "mouth")
-                             <*> parsePart (parts ! "hair")
-                             <*> parsePart (parts ! "backHair")
+                    parts <- v .: "parts" <?> Key "parts" :: Parser Object
+                    let helper n = parsePart (parts ! n) <?> Key n
 
+                    FaceFile <$> helper "contour"
+                             <*> helper "leftEye"
+                             <*> helper "rightEye"
+                             <*> helper "nose"
+                             <*> helper "mouth"
+                             <*> helper "hair"
+                             <*> helper "backHair"
 
 
 parsePart :: Value -> Parser UnloadedPart
-parsePart = withObject "Part" $ \v -> (,) <$> v .: "path"
-                                          <*> parseTuple (v ! "offset")
+parsePart = withObject "Part" $ \v -> (,) <$> v .: "path" <?> Key "path"
+                                          <*> parseTuple (v ! "offset") <?> Key "offset"
 
 -- | Parse Yaml's List to tuple '(,)'
 --
